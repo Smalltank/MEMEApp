3ed4dd2f47e43820f7895f1da8bc5577945e635f
Matteo Vigoni <mattevigo@gmail.com>
2015-11-08 16:16:54 +0100

    MEMEApp Frist commit

---
+++ GalleryApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GalleryApp
+//
+//  Created by Matteo Vigoni on 08/11/15.
+//  Copyright © 2015 Matteo Vigoni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
